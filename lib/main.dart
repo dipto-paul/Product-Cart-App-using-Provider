11af4bb7c695ad492ce981e024a6b967919f815b
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-25 13:56:36 +0600

    modified product model

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/cart_provider.dart';
+import 'screens/product_list_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home:  MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping Cart',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const ProductListScreen(),
     );
   }
 }
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -2,13 +2,12 @@ class Product {
   final int id;
   final String name;
   final double price;
-  final String image;
+  final String icon;
 
-
-  Product({
+   Product({
     required this.id,
     required this.name,
     required this.price,
-    required this.image,
-});
+    required this.icon,
+  });
 }
